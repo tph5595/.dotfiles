f7319169e17ea70fcf6bad7f02c8d1c9c8b086f5
Taylor Henderson <tph5595@users.noreply.github.com>
2020-05-01 13:12:37 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,10 +3,4 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ./update.sh
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
-npm install vtop -g
-
-# Install wtf
-go get -u github.com/wtfutil/wtf
-cd $GOPATH/src/github.com/wtfutil/wtf
-make install
-make run
+#npm install vtop -g
