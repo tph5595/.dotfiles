b77e05efdaf545926077129516585f4a6effe8e8
Taylor Henderson <tph5595@users.noreply.github.com>
2020-05-01 13:18:56 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Setup vim
+cp ../vim/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-./update.sh
+
+vim +PluginInstall +qall
+
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
-#npm install vtop -g
