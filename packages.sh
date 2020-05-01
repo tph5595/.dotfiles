2dab783919e7ec790f8ef9f06154ae295bfade36
Taylor Henderson <tph5595@users.noreply.github.com>
2020-05-01 13:02:32 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install cmake zsh vim tmux entr htop python-pip evince texlive-full glibc-doc manpages-posix-dev pandoc zathura
-pip install glances jupyterlab
+sudo apt install cmake zsh vim tmux entr htop python3-pip evince texlive-full glibc-doc manpages-posix-dev pandoc zathura
+pip3 install glances jupyterlab
 
 #for mac terminal-notifier Caskroom/cask/mactex
