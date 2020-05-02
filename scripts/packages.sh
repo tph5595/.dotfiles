fe3ea5deee7f069a2b270d6e28d87ef12fd5c2cc
Taylor Henderson <tph5595@users.noreply.github.com>
2020-05-01 22:55:39 -0400

    added ranger install

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install cmake zsh neovim tmux entr htop python3-pip evince texlive-full glibc-doc manpages-posix-dev pandoc zathura curl
+sudo apt install cmake zsh neovim tmux entr htop python3-pip evince texlive-full glibc-doc manpages-posix-dev pandoc zathura ranger curl
 pip3 install glances jupyterlab
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
