59d4a81b375c52c8acf31ee33a4ae80809192a00
Taylor Henderson <tph5595@users.noreply.github.com>
2020-05-01 23:08:29 -0400

    change jedi typo

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt install cmake zsh neovim tmux entr htop python3-pip evince texlive-full glibc-doc manpages-posix-dev jedi pandoc zathura ranger curl
-pip3 install glances jupyterlab
+sudo apt install cmake zsh neovim tmux entr htop python3-pip evince texlive-full glibc-doc manpages-posix-dev pandoc zathura ranger curl
+pip3 install glances jupyterlab jedi
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
