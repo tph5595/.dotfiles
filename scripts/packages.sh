fb70daf61335d14dbce7ebdd97c85896ea9db122
Taylor Henderson <tph5595@users.noreply.github.com>
2020-06-09 09:56:56 -0400

    add fzf support

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,6 +2,19 @@
 sudo apt install cmake zsh neovim tmux entr htop python3-pip evince texlive-full glibc-doc manpages-posix-dev pandoc zathura ranger curl
 pip3 install glances jupyterlab jedi
 
+# https://www.chrisatmachine.com/Neovim/08-fzf/
+sudo apt install fzf
+
+sudo apt install ripgrep
+
+sudo apt install universal-ctags
+
+sudo apt install silversearcher-ag
+
+sudo apt install fd-find
+
+# Install ZSH
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #for mac terminal-notifier Caskroom/cask/mactex
