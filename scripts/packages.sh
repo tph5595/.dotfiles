14ed9f23a6b739c3594ab8adf1892e2f34a3b068
Taylor Henderson <tph5595@users.noreply.github.com>
2021-01-04 11:05:48 -0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt install cmake zsh neovim tmux entr htop python3-pip evince \
 texlive-full glibc-doc manpages-posix-dev pandoc zathura ranger curl nodejs feh \
-qalc
+qalc suckless-tools
 pip3 install glances jupyterlab jedi
 
 # https://www.chrisatmachine.com/Neovim/08-fzf/
