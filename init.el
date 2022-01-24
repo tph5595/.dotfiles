10ac63995e5cc0684c99cd6948b1d7893971b225
Taylor Henderson <tph5595@verizon.net>
2022-01-24 18:01:40 -0500

    Added go support

--- init.el
+++ init.el
@@ -205,13 +205,61 @@
    #'TeX-revert-document-buffer)
 (pdf-tools-install)
 
-
+;; Python Setup
 (setq python-shell-interpreter "ipython3")
 (setq org-confirm-babel-evaluate nil)
 (org-babel-do-load-languages
 'org-babel-load-languages
 '((python . t)))
 
+;; --------------------------------------------------------------------------------------
+;; Path Share with emacs
+(use-package exec-path-from-shell)
+
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$"
+                          ""
+                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq eshell-path-env path-from-shell) ; for eshell users
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+;; --------------------------------------------------------------------------------------
+;; GoLang setup
+(use-package go-mode)
+;; Set GOPATH
+;; (setenv "GOPATH" "/Users/taylor/go")
+;; Call gofmt on save
+(add-to-list 'exec-path "/Users/tleyden/Development/gocode/bin")
+;; (add-hook 'before-save-hook 'gofmt-before-save)
+;; Set up custom state
+(defun my-go-mode-hook ()
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (local-set-key (kbd "C-c C-c") 'compile)
+)
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+;; Autocomplete setup for Go
+(use-package auto-complete)
+(use-package go-autocomplete)
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+;; Error fix
+(with-eval-after-load 'go-mode
+   (require 'go-autocomplete))
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+(exec-path-from-shell-copy-env "GOPATH")
 
 
 (custom-set-variables
