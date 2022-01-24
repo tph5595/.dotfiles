450048c45a86297bab1ce098c60b2050a34febb5
Taylor Henderson <tph5595@verizon.net>
2022-01-24 12:45:58 -0500

    Update go_setup.sh

--- go_setup.sh
+++ go_setup.sh
@@ -4,6 +4,6 @@
 # Install go doc
 go get -v  golang.org/x/tools/cmd/godoc
 # Go def
-go get github.com/rogpeppe/godef
+go install github.com/rogpeppe/godef@latest
 # Better autocomplete
 go get -u github.com/nsf/gocode
