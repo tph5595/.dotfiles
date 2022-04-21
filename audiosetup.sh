dff5b9e30446e1092da571213068852556c02978
Taylor Henderson <thender8@gmu.edu>
2022-04-21 11:57:39 -0400

    Update audiosetup.sh

--- audiosetup.sh
+++ audiosetup.sh
@@ -10,4 +10,5 @@
 pactl load-module module-jack-sink channels=2; pactl load-module module-jack-source channels=2; pacmd set-default-sink jack_out
 # Setup Scarlett ouputs
 alsa_out -d hw:$(cat /proc/asound/cards | grep "USB-Audio - Scarlett" | cut -d' ' -f2) &
+alsa_out -d hw:$(cat /proc/asound/cards | grep "Schiit" | cut -d' ' -f2) &
 qjackctl &
