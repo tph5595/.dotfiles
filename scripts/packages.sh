46783442a23d5357e9a09fc6c2c264dfda9e4d3d
Taylor Henderson <thender8@gmu.edu>
2023-02-22 16:03:18 -0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -16,3 +16,6 @@ sudo apt install libx11-dev libxft-dev libxinerama-dev
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #for mac terminal-notifier Caskroom/cask/mactex
+
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
