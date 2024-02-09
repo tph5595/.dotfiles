96a2c9a1802b788929ceb73e3e26691ccad608ac
Taylor Henderson <tph5595@verizon.net>
2024-02-09 18:09:32 -0500

    add zenith

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 sudo pacman -Syu cmake zsh neovim tmux entr htop python python-pip python-pipx \
 texlive-meta man-pages pandoc zathura curl nodejs wayland sway foot \
-duplicity ripgrep rustup
+duplicity ripgrep rustup zenith
 
 # Install rust
 rustup default stable
