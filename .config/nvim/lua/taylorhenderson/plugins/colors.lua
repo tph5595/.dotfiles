7a18bc3a290595f0e26c2e8c314ff637c5ab299e
Taylor Henderson <tph5595@verizon.net>
2024-02-14 14:20:55 -0500

    more wayland things

--- .config/nvim/lua/taylorhenderson/plugins/colors.lua
+++ .config/nvim/lua/taylorhenderson/plugins/colors.lua
@@ -3,8 +3,8 @@ function ColorMyPencils(color)
 	vim.cmd.colorscheme(color)
 
 	-- Enable below for transparent background
-	-- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	-- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 end
 
 return{
