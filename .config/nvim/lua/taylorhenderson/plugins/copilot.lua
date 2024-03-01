7211d8e75948c43ad40a0f6ef663765eb01e4aa4
Taylor Henderson <tph5595@verizon.net>
2024-03-01 11:30:43 -0500

    better copilot and minor changes

--- .config/nvim/lua/taylorhenderson/plugins/copilot.lua
+++ .config/nvim/lua/taylorhenderson/plugins/copilot.lua
@@ -1,4 +1,4 @@
-local copilot_on = true
+local copilot_on = false
 vim.api.nvim_create_user_command("CopilotToggle", function()
 	if copilot_on then
 		vim.cmd("Copilot disable")
@@ -13,8 +13,9 @@ end, { nargs = 0 })
 return {
     {
         "github/copilot.vim",
-        config = function ()
-            vim.keymap.set("n", "<leader>c", "<Cmd>CopilotToggle<CR>")
-        end
+        lazy = true,
+        keys = {
+            { "<leader>c", "<cmd>CopilotToggle<cr>", desc = "Copilot toggle" },
+        },
     }
 }
--- .config/nvim/lua/taylorhenderson/plugins/init.lua
+++ .config/nvim/lua/taylorhenderson/plugins/init.lua
@@ -19,39 +19,21 @@ return {
             require("fidget").setup({})
         end
     },
-    -- {
-    --     'VonHeikemen/lsp-zero.nvim',
-    --     branch = 'v1.x',
-    --     dependencies = {
-    --         -- LSP Support
-    --         {'neovim/nvim-lspconfig'},
-    --         {'williamboman/mason.nvim'},
-    --         {'williamboman/mason-lspconfig.nvim'},
-
-    --         -- Autocompletion
-    --         {'hrsh7th/nvim-cmp'},
-    --         {'hrsh7th/cmp-buffer'},
-    --         {'hrsh7th/cmp-path'},
-    --         {'saadparwaiz1/cmp_luasnip'},
-    --         {'hrsh7th/cmp-nvim-lsp'},
-    --         {'hrsh7th/cmp-nvim-lua'},
-
-    --         -- Snippets
-    --         {'L3MON4D3/LuaSnip'},
-    --         {'rafamadriz/friendly-snippets'},
-    --     }
-   -- },
- "tpope/vim-repeat",
- {
-     "folke/zen-mode.nvim",
-     config = function ()
-         vim.keymap.set("n", "<leader><leader>", function ()
-             require("zen-mode").toggle({
-                 window = {
-                     width = .85 -- width will be 85% of the editor width
-                 }
-             })
-         end)
-     end
- }
+    "tpope/vim-repeat",
+    {
+        "folke/zen-mode.nvim",
+        config = function ()
+            vim.keymap.set("n", "<leader><leader>", function ()
+                require("zen-mode").toggle({
+                    window = {
+                        width = .85 -- width will be 85% of the editor width
+                    }
+                })
+            end)
+        end
+    },
+    -- Good markdown stuff <leader>e for code block editing with syntax highitng in
+    -- sepereate window
+    -- "gabrielelana/vim-markdown"
+    "nvim-tree/nvim-web-devicons",
 }
